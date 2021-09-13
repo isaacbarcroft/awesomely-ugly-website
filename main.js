b9adc2cbbf054bfc7f11b82f07e36941602801a3
isaacbarcroft <i_barcroft@yahoo.com>
2021-09-13 13:03:02 -0400

    html elements

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('hello');
+let button = document.querySelector('.button');
+let img = document.querySelector('.img');
+button.addEventListener('click', function(event){
+
+    if (img.src = "https://placekitten.com/200/300"){
+        img.src = "https://placekeanu.com/200/150"
+} else if (img.src ="https://placekeanu.com/200/150"){
+    img.src = "https://placekitten.com/200/300";
+}
+});
