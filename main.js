fe67fc257836c960b9d778a52204ef1985c62fd7
isaacbarcroft <i_barcroft@yahoo.com>
2021-09-14 09:10:22 -0400

    Fixed Img change

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 let button = document.querySelector('.button');
 let img = document.querySelector('.img');
 let footer = document.getElementById('footer');
-console.log(footer);
+
+
+
 footer.addEventListener('mouseover', function(event){
     event.target.innerText = 'Thanks for viewing';
 })
 button.addEventListener('click', function(event){
-
-    if (img.src = "https://placekitten.com/200/300"){
-        img.src = "https://placekeanu.com/200/150"
-} else if (img.src ="https://placekeanu.com/200/150"){
+console.log(img.src);
+    if (img.src === "https://placekitten.com/200/300"){
+        img.src = "https://placekeanu.com/200/150";
+} else if (img.src === "https://placekeanu.com/200/150"){
     img.src = "https://placekitten.com/200/300";
 }
 });
